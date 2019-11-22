df95e38ad21a06be47a7406b9394227d7ac42d37
Higor Santos <higor.dantas@ccc.ufcg.edu.br>
2019-11-21 23:34:40 -0300

    created an organized home

--- js/main.js
+++ js/main.js
@@ -1,96 +1,112 @@
-// import User from './user.js';
+import User from './user.js';
 
 let url = 'https://apiajude.herokuapp.com/api';
+let $viewer = document.querySelector('#viewer');
 
-//let $viewer = document.querySelector('#viewer');
-let $box = document.querySelector("#box");
+window.onload = viewerChange;
+window.addEventListener('hashchange', viewerChange);
 
-// let registerUserBtn = document.querySelector('#register-button');
-// registerUserBtn.addEventListener('click', createUser);
-//
-// function createUser() {
-//     let firstNameInput = document.querySelector("#user-first-name");
-//     let lastNameInput = document.querySelector("#user-last-name");
-//     let emailInput = document.querySelector("#user-email");
-//     let creditCardInput = document.querySelector("#user-credit-card");
-//     let passwordInput = document.querySelector("#user-password");
-//
-//     let u = new User(
-//         firstNameInput.value,
-//         lastNameInput.value,
-//         emailInput.value,
-//         creditCardInput.value,
-//         passwordInput.value
-//     );
-//
-//     firstNameInput.value = "";
-//     lastNameInput.value = "";
-//     emailInput.value = "";
-//     creditCardInput.value = "";
-//     passwordInput.value = "";
-//
-//     registerUser(u);
-// }
-//
-// function showConfirmView() {
-//     let $body = document.querySelector('body');
-//     let $div = document.createElement('div');
-//     let $p = document.createElement('p');
-//     let $img = document.createElement('img');
-//
-//     $div.className = 'opaque-div flex-box';
-//     $div.id = 'confirm-view'
-//     $p.innerText = "Você agora está cadastrado!";
-//     $img.id = 'check-img';
-//     $img.src = 'images/check.svg';
-//     $img.style.filter = 'invert(100%)';
-//
-//     $div.appendChild($img);
-//     $div.appendChild($p);
-//     $body.appendChild($div);
-// }
-//
-// function showFailureView() {
-//     let $body = document.querySelector('body');
-//     let $div = document.createElement('div');
-//     let $p = document.createElement('p');
-//     let $img = document.createElement('img');
-//
-//     $div.className = 'opaque-div flex-box';
-//     $div.id = 'confirm-view'
-//     $p.innerText = "Opa! Parece que você já está cadastrado...";
-//     $img.id = 'check-img';
-//     $img.src = 'images/fail.svg';
-//     $img.style.filter = 'invert(100%)';
-//
-//     $div.appendChild($img);
-//     $div.appendChild($p);
-//     $body.appendChild($div);
-// }
-//
-// async function registerUser(user) {
-//     try {
-//         let body = JSON.stringify(user);
-//         let header = {
-//             'Content-Type': 'application/json;charset=utf-8'
-//         };
-//
-//         let response = await fetch(url + '/user', {
-//             'method': 'POST',
-//             'body': body,
-//             'headers': header
-//         });
-//
-//         if (response.status == 201) {
-//             showConfirmView();
-//         } else if (response.status == 400) {
-//             showFailureView();
-//         }
-//     } catch (e) {
-//         console.log(e);
-//     }
-// }
+function viewerChange() {
+    let hash = location.hash;
 
+    if ([''].includes(hash)) {
+        $viewer.innerHTML = '';
+    } else if (['#user-register'].includes(hash)) {
+        viewUserRegister();
+    }
+}
+
+function createUser() {
+    let firstNameInput = document.querySelector("#user-first-name");
+    let lastNameInput = document.querySelector("#user-last-name");
+    let emailInput = document.querySelector("#user-email");
+    let creditCardInput = document.querySelector("#user-credit-card");
+    let passwordInput = document.querySelector("#user-password");
+
+    let u = new User(
+        firstNameInput.value,
+        lastNameInput.value,
+        emailInput.value,
+        creditCardInput.value,
+        passwordInput.value
+    );
+
+    firstNameInput.value = "";
+    lastNameInput.value = "";
+    emailInput.value = "";
+    creditCardInput.value = "";
+    passwordInput.value = "";
+
+    registerUser(u);
+}
+
+function showConfirmView() {
+    let $body = document.querySelector('body');
+    let $div = document.createElement('div');
+    let $p = document.createElement('p');
+    let $img = document.createElement('img');
+
+    $div.className = 'opaque-div flex-box';
+    $div.id = 'flex-box-column'
+    $p.innerText = "Você agora está cadastrado!";
+    $img.id = 'check-img';
+    $img.src = 'images/check.svg';
+    $img.style.filter = 'invert(100%)';
+
+    $div.appendChild($img);
+    $div.appendChild($p);
+    $body.appendChild($div);
+}
+
+function showFailureView() {
+    let $body = document.querySelector('body');
+    let $div = document.createElement('div');
+    let $p = document.createElement('p');
+    let $img = document.createElement('img');
+
+    $div.className = 'opaque-div flex-box';
+    $div.id = 'flex-box-column'
+    $p.innerText = "Opa! Parece que você já está cadastrado...";
+    $img.id = 'check-img';
+    $img.src = 'images/fail.svg';
+    $img.style.filter = 'invert(100%)';
+
+    $div.appendChild($img);
+    $div.appendChild($p);
+    $body.appendChild($div);
+}
+
+function viewUserRegister() {
+    let $template = document.querySelector('#view-user-register');
+    $viewer.innerHTML = $template.innerHTML;
+
+    let $registerUserBtn = document.querySelector('#register-button');
+    $registerUserBtn.addEventListener('click', createUser);
+}
+
+async function registerUser(user) {
+    try {
+        let body = JSON.stringify(user);
+        let header = {
+            'Content-Type': 'application/json;charset=utf-8'
+        };
+
+        let response = await fetch(url + '/user', {
+            'method': 'POST',
+            'body': body,
+            'headers': header
+        });
+
+        if (response.status == 201) {
+            showConfirmView();
+        } else if (response.status == 400) {
+            showFailureView();
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
+/*
 let $listingCampaignsTemplate, $home;
 async function fetch_templates() {
     let listingCampaignsTemplate = await (fetch('/html/listingCampaignsTemplate.html').then(r => r.text()));
@@ -105,14 +121,14 @@ async function fetch_templates() {
     $home = div.querySelector('#home');
 }
 
-(async function main(){
+(async function main() {
 
     await Promise.all([fetch_templates()]);
     let hash = location.hash;
 
     $box.innerHTML = "";
 
-    if (["","#home"].includes(hash)){
+    if (["", "#home"].includes(hash)) {
         home();
     }
     else if (["#listagem"].includes(hash)) {
@@ -121,16 +137,16 @@ async function fetch_templates() {
 
 }());
 
-function home()
-{
+
+function home() {
     $box.innerHTML = $home.innerHTML;
     let $a = document.querySelector('a');
     $a.addEventListener('click', listingCampaigns);
 }
 
-function listingCampaigns()
-{
+function listingCampaigns() {
     $box.innerHTML = $listingCampaignsTemplate.innerHTML;
     let $a = document.querySelector('a');
     $a.addEventListener('click', home);
 }
+*/
